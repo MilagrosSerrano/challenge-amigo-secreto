1ee0c2c60f6875239084aed215eacf86f5f77a37
MilagrosSerrano <serranomilagros23@gmail.com>
2025-07-29 11:50:19 -0300

    finaliza logica de sorteo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# challenge-amigo-secreto
-Challenge de ONE Education para poner en práctica conceptos de lógica de programación.
+# Challenge Amigo Secreto
+
+Se trata de un seleccionador aleatorio de nombres, dado una lista de nombres, los cuales son ingresados por el usuario en un campo de texto, se genera un número aleatorio con el cual se elige al nombre sorteado: el amigo secreto.
+
+Aquí se muestra un ejemplo de como utilizar la página:
+
+
+
--- app.js
+++ app.js
@@ -1,24 +1,55 @@
-/* Fucionalidades:
-Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
-
-Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
-
-Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
-
-Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página. */
-
+//inicialización de variables
+let amigos = [];
+let amigo = "";
 
 //al hacer click en botón añade el nombre a la lista
-function agregarAmigo(){
-
+function agregarAmigo() {
+  amigo = document.getElementById("amigo").value;
+  if (amigo == "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    amigos.push(amigo);
+    limpiaInput();
+    mostrarAmigos();
+  }
+  return;
 }
 
+//limpia el input de texto luego de añadir el nombre a la lista
+function limpiaInput() {
+  let input = document.getElementById("amigo");
+  input.value = "";
+  return;
+}
+
+//recorre la lista de amigos y se muestran en pantalla
+function mostrarAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    listaAmigos.innerHTML += `<li> ${amigos[i]} </li>`;
+  }
+  return;
+}
+
+//vacía la lista de los nombres luego del sorteo, para que solo se muestre el nombre sorteado
+function vaciarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    return;
+}
 
 
 //al hacer click sortea un amigo de manera aleatoria entre todos los de la lista
-function sortearAmigo(){
-
+function sortearAmigo() {
+    if (amigos.length == 0){
+        alert('No hay ningún amigo en la lista, ingrese nombres para luego sortear.');
+    }else{
+        let numAmigoSeleccionado = Math.floor(Math.random()*amigos.length);
+        let nombreSorteado  = amigos[numAmigoSeleccionado];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `El amigo secreto sorteado es: ${nombreSorteado}`;
+        vaciarLista();
+    }
+    return;
 }
-
-
-
